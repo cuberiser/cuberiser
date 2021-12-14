17cd6f8649047103996a98d1412e15be96a6fa83
cuberiser <cuberiser@gmail.com>
2021-12-14 08:31:20 +0530

    Add label

--- __main__.py
+++ __main__.py
@@ -1,7 +1,10 @@
-from tkinter import Tk
+from tkinter import Tk, Label
 
 root = Tk()
 root.title("subscribe to cube riser")
 root.geometry("640x360")
+
+label = Label(root, text="Subscribe to Cube Riser", font=("Cascadia Code", 30))
+label.grid(row=0, column=0)
 
 root.mainloop()
